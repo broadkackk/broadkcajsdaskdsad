c4bca7b19db2ef27f055fd6b486ac19a00dc37db
broadkackk <42350706+broadkackk@users.noreply.github.com>
2018-08-13 17:56:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,23 +27,6 @@ client.on('ready', () => {
   console.log('')
 });
 
-
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
-});
-
-
-
 client.on('message', message => {
 var prefix = "!";
  
@@ -69,4 +52,17 @@ if (message.content.startsWith(prefix + 'setimg')) {
 }
 });
 
+client.on("message", message => {
+
+            if (message.content.startsWith(prefix + "bc")) {
+                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+  let args = message.content.split(" ").slice(1);
+  var argresult = args.join(' '); 
+  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
+ m.send(`${argresult}\n ${m}`);
+})
+ message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
+ message.delete(); 
+};     
+});
 client.login(process.env.BOT_TOKEN);
